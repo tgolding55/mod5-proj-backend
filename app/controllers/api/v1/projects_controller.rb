3d373907aca960b8efd81ecd2c7b4138482dc8a3
Tim Golding <tgolding55@hotmail.com>
2019-12-13 14:34:34 +0000

    refactor

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::ProjectsController < ApplicationController
     def show
         project = Project.find(params["id"])
         collaborators = project.collaborators.map{|collaborator| UserSerializer.new(collaborator.user)}
-        comments = project.project_comments.map{|comment| {id:comment.id, content: comment.content, likes: comment.comment_likes, user: {username:comment.user.username, id: comment.user.id}}}.reverse
+        comments = project.project_comments.map{|comment| {comment_id:comment.id, content: comment.content, likes: comment.comment_likes, user: {username:comment.user.username, id: comment.user.id}}}.reverse
         serializedProject = ProjectSerializer.new(project)
         render json: {project: serializedProject, collaborators: collaborators, comments: comments}, status: :ok
     end
